935559debaacb1f1aeb810d494c23b3c4f77548a
Jacob Stover <jacob.stover@ahead.com>
2025-06-18 08:21:13 -0400

    chore: add live view example

--- lib/watercooler_web/live/home_live/index.ex
+++ lib/watercooler_web/live/home_live/index.ex
@@ -4,13 +4,33 @@ defmodule WatercoolerWeb.HomeLive.Index do
   use WatercoolerWeb, :live_view
 
   def mount(_params, _session, socket) do
+    socket = assign(socket, :count, 0)  
+
+    if connected?(socket) do
+      IO.inspect(socket, label: "================== SOCKET\n")
+      # expensive db operation
+    else
+      IO.inspect(label: "================== NOT CONNECTED\n")
+    end
+
     {:ok, socket}
+  end
+
+  def handle_event("increment", _, socket) do
+    socket = update(socket, :count, &(&1 + 1))
+    {:noreply, socket}
   end
 
   def render(assigns) do
     ~H"""
     <div>
       WatercoolerWeb.HomeLive.Index
+
+      <.button variant="primary" phx-click="increment">Increment</.button>
+      <.button variant="secondary" phx-click="increment">Increment</.button>
+
+      <div>count: {@count}</div>
+      <div>elixir: {1 + 1}</div>
     </div>
     """
   end
